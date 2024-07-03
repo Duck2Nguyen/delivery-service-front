054f949fddd6f9c5a7f2d2792d0fd96ed467e1b4
hai.nd <duchai2212001@gmail.com>
2024-07-03 23:07:47 +0700

    change IP backend to https

--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -10,7 +10,7 @@ import axios from "axios";
 // });
 
 const api = axios.create({
-  baseURL: "http://34.142.165.57:8081/api/",
+  baseURL: "https://34.142.165.57:8081/api/",
   headers: {
     "Content-type": "application/json",
   },
